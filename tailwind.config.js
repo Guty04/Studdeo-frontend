8f28b05fa76d71ef817e0b5b1800f3557eac90d7
Martín <91429@sistemas.frc.utn.edu.ar>
2026-01-12 19:57:06 -0300

    fix/Prueba de tw Animated separated de testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -81,6 +83,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
